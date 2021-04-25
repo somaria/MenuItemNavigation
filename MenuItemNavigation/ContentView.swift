b791baf9f6ce1c57a017ba535b383791967cd9be
Daryl <daryl.9@gmail.com>
2021-04-25 13:28:49 +0800

    Initial Commit

---
+++ MenuItemNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MenuItemNavigation
+//
+//  Created by admin on 25/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MenuItemNavigation/MenuItemNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuItemNavigationApp.swift
+//  MenuItemNavigation
+//
+//  Created by admin on 25/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MenuItemNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
